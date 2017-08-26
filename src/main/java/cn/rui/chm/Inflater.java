c283d9629fb16684fb4d8bbf74a5d84add2f539a
viruscamp <viruscamp@gmail.com>
2017-08-27 00:07:33 +0800

    update to https://sourceforge.net/projects/chmpane/files/CHMPane-alpha/CHMPane-alpha2/CHMPane_alpha2_bsd.zip/download change LICENSE to BSD like

--- src/main/java/cn/rui/chm/Inflater.java
+++ src/main/java/cn/rui/chm/Inflater.java
@@ -2,27 +2,51 @@
  * Copyright (C) 2007 Rui Shen (rui.shen@gmail.com) All Right Reserved
  * File     : Inflater.java
  * Created	: 2007-3-1
+ * 
  * ****************************************************************************
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * Copyright (C) 2007 Rui Shen (rui.shen@gmail.com)
+ * http://chmpane.sourceforge.net, All Right Reserved. 
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "CHMPane" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *****************************************************************************
- */package cn.rui.chm;
+ */
+package cn.rui.chm;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
+/**
+ * The LZX inflation code here is constructed by reading Jed Wing's C code.
+ * 
+ * @author <a href="mailto:rui.shen@gmail.com">Rui Shen</a>
+ *
+ */
 class Inflater {
 	 
 	private static Logger log = Logger.getLogger(Inflater.class.getName());
