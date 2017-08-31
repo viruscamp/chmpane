ec2707171f3495ece82dde7c27181e130579cad4
viruscamp <viruscamp@gmail.com>
2017-08-31 16:49:02 +0800

    add application.yml for config

--- chmpane-springmvc-demo/src/main/java/net/sf/chmpane/springmvc/Application.java
+++ chmpane-springmvc-demo/src/main/java/net/sf/chmpane/springmvc/Application.java
@@ -1,17 +1,23 @@
 package net.sf.chmpane.springmvc;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableWebMvc
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -22,6 +28,24 @@ public class Application {
 
     @PostConstruct
     public void init() throws IOException {
-        chmController.addMapping("7z", new File("C:\\Program Files\\7-Zip\\7-zip.chm"));
+        if (chmConfig.getMappings() != null) {
+            for (Map.Entry<String, String> e : chmConfig.getMappings().entrySet()) {
+                try {
+                    chmController.addMapping(e.getKey(), new File(e.getValue()));
+                } catch (Exception ex) {
+                    log.error(MessageFormat.format("chmController.addMapping fail: {0} {1}", e.getKey(), e.getValue()), ex);
+                }
+            }
+        }
+    }
+
+    @Autowired
+    private ChmConfig chmConfig;
+
+    @Component
+    @ConfigurationProperties(prefix = "chm")
+    @Data
+    public static class ChmConfig {
+        private Map<String, String> mappings;
     }
 }
